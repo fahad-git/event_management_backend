c52aed2f121de40571be3e93e786395423980311
fahad-git <fahadsiddiqui056@gmail.com>
2021-03-13 17:36:08 +0500

    session implemented, now modifying for jwt token

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// var cookieParser = require('cookie-parser');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+const cors = require('cors');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { signedCookie } = require('cookie-parser');
 
 var app = express();
 
@@ -13,12 +18,25 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// THE Secret key will be user by cookie parser to encrypt cookies
+// app.use(cookieParser("12345-67890-09876-54321"));
 
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+
+// app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -39,3 +57,99 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+// Authorization
+
+// function auth(req, res, next){
+//   // console.log(req.headers);
+//   // console.log(req.signedCookies);
+
+//   // if(!req.signedCookies.user){
+    
+//   if(!req.session.user){
+//     var authHeader = req.headers.authorization;
+    
+  
+//     if(!authHeader){
+//       var err = new Error('You are not authenticated!');
+      
+//       res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 401;
+//       return next(err);  
+//     }
+
+//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     var username = auth[0];
+//     var password = auth[1];
+//     let record = login(connection, 'SELECT * FROM login WHERE id = 2');
+//     console.log("Record:");
+//     console.log(record);
+//     if(username === 'admin' && password === '123'){
+//       // res.cookie("user", 'admin', { signed: true })
+//       req.session.user = 'admin';
+//       next();
+//     }else{
+//       var err = new Error('You are not authenticated!');
+  
+//       res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 401;
+//       return next(err);
+//     }
+  
+//   }else{
+//     // if(req.signedCookies.user === 'admin'){
+      
+//     if(req.session.user === 'admin'){
+//       next();
+//     }else{
+//       var err = new Error('You are not authenticated!');
+  
+//       res.setHeader('WWW-Authenticate', 'Basic');
+//       err.status = 401;
+//       return next(err);
+//     }
+//   }
+
+
+// }
+
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'ems'
+// })
+
+// connection.connect(function(err) {
+//   if (err) 
+//   {
+//     console.log(err);
+//     return;
+//   }
+//   console.log("Database Connected Successfully!");
+// });
+
+// connection.end()
+
+// const login = (conn, query) => {
+//   try{
+//   // conn.connect()
+
+//   conn.query(query, function (err, rows, fields) {
+//       if (err) {
+//         console.log(err)
+//         return;
+//       }
+//       // console.log(rows)s
+//       // console.log(fields);
+//       return rows
+//     })
+
+//   // conn.end()
+//   }
+//   catch(e){
+//     console.log(e);
+//   }
+// }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,148 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  next();
+});
+
+// module.exports = router;
+
+// DB Connection
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ems'
+})
+
+connection.connect(function(err) {
+  if (err) 
+  {
+    console.log(err);
+    return;
+  }
+  console.log("Database Connected Successfully!");
+});
+
+
+// router.post('/signup', (req, res, next) => {
+  
+//   conn.query(query, function (err, rows, fields) {
+//     if (err) {
+//       console.log(err)
+//       return;
+//     }
+//     // console.log(rows)
+//     // console.log(fields);
+//     return rows
+//   })
+//     .then((user) => {
+//     if(user != null) {
+//       var err = new Error('User ' + req.body.username + ' already exists!');
+//       err.status = 403;
+//       next(err);
+//     }
+//     else {
+//       return User.create({
+//         username: req.body.username,
+//         password: req.body.password});
+//     }
+//   })
+//   .then((user) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.json({status: 'Registration Successful!', user: user});
+//   }, (err) => next(err))
+//   .catch((err) => next(err));
+// });
+
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+});
+
+
+/*
+
+router.post('/login', (req, res, next) => {
+  console.log(req.headers)
+  if(!req.session.user) {
+    var authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0].toString().trim();
+    var password = auth[1].toString().trim();
+    
+    if(username === '' || password === ''){
+      var err = new Error('Empty username or password!');
+      err.status = 403;
+      return next(err);
+    }
+
+    let query = "SELECT * FROM login WHERE username = '" + username +"' LIMIT 1";
+    console.log(query)
+    connection.query(query, function (err, user, fields) {
+      if (err) {
+        console.log(err)
+        return;
+      }
+
+      user = user[0];
+
+      if(user === null){
+        var err = new Error('User ' + username + ' does not exist!');
+        err.status = 403;
+        return next(err);
+      }else if (user.password !== password) {
+        var err = new Error('Your password is incorrect!');
+        err.status = 403;
+        return next(err);
+      }else if (user.username === username && user.password === password) {
+        req.session.user = 'authenticated';
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('You are authenticated!')
+      }
+
+    })//query ends
+    
+  }
+  else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You are already authenticated!');
+  }
+})
+
+*/
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
 });
 
 module.exports = router;
