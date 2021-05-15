6979a782cd0d1b3bfb5a9d16fd1e520b579c91d9
fahad-git <fahadsiddiqui056@gmail.com>
2021-05-15 18:12:23 +0500

    Before live the product

--- dbHandler.js
+++ dbHandler.js
@@ -21,6 +21,7 @@ function handleDisconnect() {
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      return;
     }            
     console.log("Database Connected Successfully!");                         // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
