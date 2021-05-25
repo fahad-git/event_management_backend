6988dcffc39a5b38c71d0089c195169244e9cc6f
fahad-git <fahadsiddiqui056@gmail.com>
2021-05-25 15:08:07 +0500

    Deploying build

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-
+/*
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'allowedOrigins':['http://localhost:3000', 'https://localhost:3443', 
@@ -11,17 +11,17 @@ module.exports = {
         multipleStatements: true
       },
 }
+*/
 
-
-// module.exports = {
-//   'secretKey': '12345-67890-09876-54321',
-//   'allowedOrigins':['http://localhost:3000', 'https://localhost:3443', 'http://localhost:5000', 'http://192.168.1.100:3000',  'http://192.168.1.105:3000', 'http://3.137.205.27:3000'],
-//   'sqlConfigurations' : {
-//       host: 'ems-database.czq7a26zc6o2.eu-west-2.rds.amazonaws.com',
-//       user: 'root',
-//       password: 'PerfektEMS',
-//       database: 'ems',
-//       port:3306,
-//       multipleStatements: true
-//     },
-// }
+module.exports = {
+  'secretKey': '12345-67890-09876-54321',
+  'allowedOrigins':['http://localhost:3000', 'https://localhost:3443', 'http://localhost:5000', 'http://192.168.1.100:3000',  'http://192.168.1.105:3000', 'http://3.137.205.27:3000'],
+  'sqlConfigurations' : {
+      host: 'ems-database.czq7a26zc6o2.eu-west-2.rds.amazonaws.com',
+      user: 'root',
+      password: 'PerfektEMS',
+      database: 'ems',
+      port:3306,
+      multipleStatements: true
+    },
+}
